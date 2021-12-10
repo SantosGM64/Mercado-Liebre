3e38cc84d0a861d8c503ec91910d3753908f5f25
Santos <sgaticamiles@gmail.com>
2021-12-10 00:30:44 -0300

    sbiendo archivos aun no esta completo

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ app.listen(app.get("port"),()=> console.log ("Servidores corriendo"))
 app.get("/", (req,res)=> res.sendFile(path.resolve(__dirname, "./views","index.html")))
 
 app.use(express.static(path.resolve(__dirname,"./public")))
+
+app.get('/', (req,res)=>{ res.sendFile(__dirname + '/views/index.html');});
+S
+app.get('/register', (req,res)=>{res.sendFile(__dirname + 'views/register.html');});
+
+app.get('/login', (req,res)=>{res.sendFile(__dirname + 'views/login.html');}); 
